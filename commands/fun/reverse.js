25b8b1cccfea7639b21904e668a0364e0a290171
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-09-20 23:24:06 +0700

    add 2 command kiss and reverse

---
+++ commands/fun/reverse.js
@@ -0,0 +1,15 @@
+module.exports = {
+    name: "reverse",
+    usage: "<text>",
+    description: "Reverse your desired words",
+    category: "fun",
+    timeout: 5,
+    alises: [""],
+    run: async(bot, message, args) => {
+        const text = args.join();
+        if(!text) return message.channel.send("Please provide a word.")
+        if(text.length < 1) return message.channel.send("BAKA, HOW CAN I REVERSE 1 WORD!!!")
+        const converted = tex.split('').reverse().join('');
+        message.channel.send(`\u180E${converted}`);
+    },
+};
