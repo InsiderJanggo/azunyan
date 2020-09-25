c0dad13d2902ed2b94a57ade4e83a06792d83ce1
angel <angelbecerrah548@gmail.com>
2020-09-25 16:15:41 -0500

    feat(*): checks & addons

--- handlers/command.js
+++ handlers/command.js
@@ -1,19 +1,28 @@
-const {readdirSync, readdir} = require('fs');
+const { readdirSync } = require('fs');
 const ascii = require('ascii-table');
-let table = new ascii("Commands");
-table.setHeading('Command','Load status');
-module.exports= (bot) => {
-    readdirSync('./commands/').forEach(dir=>{
-        const commands = readdirSync(`./commands/${dir}/`).filter(file=>file.endsWith('.js'));
-        for(let file of commands){
+const table = new ascii("Commands");
+table.setHeading('Command', 'Load status');
+
+module.exports = (bot) => {
+    readdirSync('./commands/').forEach(dir => {
+        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
+        for (let file of commands) {
             let pull = require(`../commands/${dir}/${file}`);
-            if(pull.name){
-                bot.commands.set(pull.name, pull);
-                table.addRow(file, '✅')
-            }else{
-                table.addRow(file, '❌ -> Missing a help.name or help.name is not a String')
+            try {
+                if (typeof pull.name != 'string' || typeof pull != 'object') throw new Error('Missing a name or name is not a String');
+                if (pull.category && typeof pull.category !== 'string') throw new Error('category is not a String');
+                if (pull.description && typeof pull.description !== 'string') throw new Error('description is not a String');
+                if (pull.timeout && typeof pull.timeout !== 'number') throw new Error('timeout is not a Number');
+                if (pull.usage && typeof pull.usage !== 'string') throw new Error('usage is not a String');
+                if (pull.name) {
+                    bot.commands.set(pull.name, pull);
+                    table.addRow(file, '√')
+                }
+                if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => bot.aliases.set(alias, pull.name));
+            } catch (error) {
+                table.addRow(file, `X -> ${error}`)
                 continue;
-            } if(pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => bot.aliases.set(alias, pull.name))
+            }
         }
     });
     console.log(table.toString());
---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off 
+title Program starter 
+color 0f
+echo program starting....
+node ./index.js
+pause
