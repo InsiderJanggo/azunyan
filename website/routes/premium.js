30dcc5550d356e82134c50595fefea211a0d5474
Wisly ong <52624933+InsiderJanggo@users.noreply.github.com>
2020-09-28 08:53:59 +0700

    Update premium.js

--- website/routes/premium.js
+++ website/routes/premium.js
@@ -1,5 +1,5 @@
 const app = require("express").Router();
-const Render = require('../../utils/render');
+const Render = require('@utils/render');
 
 app.get("/", (req, res, next) => {
     Render(req.bot, req, res, 'premium', {
